8f623b67d410f41293e9fc2452344246a8bb2168
PhamVanViet23130380 <23130380@st.hcmuaf.edu.vn>
2025-12-25 21:49:23 +0700

    update phần đăng kí login

--- src/main/java/vn/edu/nlu/fit/shopquanao/Service/UserService.java
+++ src/main/java/vn/edu/nlu/fit/shopquanao/Service/UserService.java
@@ -49,4 +49,3 @@ public class UserService {
         return userDao.verifyOtp(email, otp);
     }
 }
-
--- src/main/java/vn/edu/nlu/fit/shopquanao/model/User.java
+++ src/main/java/vn/edu/nlu/fit/shopquanao/model/User.java
@@ -1,72 +1,72 @@
 package vn.edu.nlu.fit.shopquanao.model;
 
 public class User {
-        private int id;
-        private String username;
-        private String email;
-        private String password;
-        private String role;
-        private int isActive;
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private int isActive;
 
-        // ===== constructor rỗng (BẮT BUỘC cho JDBI) =====
-        public User() {}
+    // ===== constructor rỗng (BẮT BUỘC cho JDBI) =====
+    public User() {}
 
-        // ===== getter / setter =====
-        public int getId() {
-            return id;
-        }
+    // ===== getter / setter =====
+    public int getId() {
+        return id;
+    }
 
-        public void setId(int id) {
-            this.id = id;
-        }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public String getUsername() {
-            return username;
-        }
+    public String getUsername() {
+        return username;
+    }
 
-        public void setUsername(String username) {
-            this.username = username;
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public String getPassword() {
-            return password;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        // map password_hash → passwordHash
-        public String getRole() {
-            return role;
-        }
+    // map password_hash → passwordHash
+    public String getRole() {
+        return role;
+    }
 
-        public void setRole(String role) {
-            this.role = role;
-        }
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     public int getIsActive() {return isActive;}
 
     public void setIsActive(int isActive) {this.isActive = isActive;}
 
-        @Override
-        public String toString() {
-            return "User{" +
-                    "id=" + id +
-                    ", name='" + username + '\'' +
-                    ", email='" + email + '\'' +
-                    ", role='" + role + '\'' +
-                    ", isActive=" + isActive +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
 
 
 }
