127cc93985f1c5046c5d23023ffc2d490ee60fdf
HuyHoang <23130115@st.hcmuaf.edu.vn>
2026-01-28 01:38:05 +0700

    Full Banner-Admin

--- src/main/java/vn/edu/nlu/fit/shopquanao/Dao/BannerDao.java
+++ src/main/java/vn/edu/nlu/fit/shopquanao/Dao/BannerDao.java
@@ -22,4 +22,57 @@ public class BannerDao extends BaseDao {
 
     }
 
+    public List<Banner> getAllBanner() {
+        return getJdbi().withHandle(handle -> handle.createQuery("""
+                SELECT *
+                FROM banners""")
+                .mapToBean(Banner.class)
+                .list());
+    }
+
+    public void createBanner(Banner banner) {
+        getJdbi().withHandle(handle -> handle.createUpdate("""
+                INSERT INTO banners(id, image_url, navigate_to, title, status, created_at)
+                VALUES(:id, :imageUrl, :navigateTo, :title, :status, NOW())""")
+                .bindBean(banner)
+                .execute());
+    }
+
+    public Banner getBannerById(int id) {
+        return getJdbi().withHandle(handle -> handle.createQuery("""
+                SELECT id, image_url, navigate_to, title, status, created_at
+                FROM banners
+                WHERE id = :id""")
+                .bind("id", id)
+                .mapToBean(Banner.class)
+                .findOne()
+                .orElse(null));
+    }
+
+    public void updateBanner(Banner banner) {
+        getJdbi().withHandle(handle -> handle.createUpdate("""
+                UPDATE banners
+                SET image_url = :imageUrl,
+                navigate_to = :navigateTo,
+                title = :title,
+                status = :status
+                WHERE id = :id
+                """).bind("imageUrl", banner.getImageUrl())
+                .bind("navigateTo", banner.getNavigateTo())
+                .bind("title", banner.getTitle())
+                .bind("status", banner.isStatus())
+                .bind("id", banner.getId())
+                .execute());
+    }
+
+
+    public void deleteBanner(int id, boolean status) {
+        getJdbi().withHandle(handle -> handle.createUpdate("""
+                UPDATE banners
+                SET status =:status
+                WHERE id = :id""")
+                .bind("status",status)
+                .bind("id",id)
+                .execute());
+    }
 }
